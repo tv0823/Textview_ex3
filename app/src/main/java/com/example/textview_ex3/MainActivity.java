d0d69420f273408be31b8075045f96a585e55dd7
tv0823 <tv0823@bs.amalnet.k12.il>
2023-12-19 09:39:19 +0200

    final

--- app/src/main/java/com/example/textview_ex3/MainActivity.java
+++ app/src/main/java/com/example/textview_ex3/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.textview_ex3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView tV;
+    int timeClicked = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+    }
+
+    public void pressed(View view) {
+        timeClicked ++;
+        tV.setTextColor(Color.GREEN);
+        if (timeClicked != 6)
+        {
+            tV.setText("This is a click number: "+timeClicked);
+        }
+        else
+        {
+            tV.setText("Enough to click. Go to new start!");
+            timeClicked = 0;
+        }
     }
 }
